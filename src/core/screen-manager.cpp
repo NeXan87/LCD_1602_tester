a7ae9932b7a746fd69e9b5325f7d36d7c103a503
NeXan <nexan87@gmail.com>
2026-01-23 17:54:29 +0300

    Меняет порядок элементов screenId

--- src/core/screen-manager.cpp
+++ src/core/screen-manager.cpp
@@ -19,24 +19,34 @@ static ScreenId updateScreen(ScreenId id) {
     switch (id) {
         case SCREEN_MAIN_MENU:
             return updateMainMenu();
+
+        // ───────────── Уровень 1: Главное меню ─────────────
+        case SCREEN_IR_TEST:
         case SCREEN_ENCODER_TEST:
             return updateScreenEncoderTest();
-        case SCREEN_ENCODER_TEST_TTL:
-            return updateScreenEncoderTTLTest();
         case SCREEN_SETTINGS:
             return updateScreenSettings();
+        case SCREEN_DIAGNOSTICS:
+            return updateScreenDiagnostics();
+        case SCREEN_ABOUT:
+            return updateAboutScreen();
+
+        // ───────────── Уровень 2: Settings ─────────────
         case SCREEN_BACKLIGHT_EDIT:
             return updateScreenBacklightEdit();
         case SCREEN_BATTERY_EDIT:
             return updateScreenBatteryEdit();
-        case SCREEN_DIAGNOSTICS:
-            return updateScreenDiagnostics();
+
+        // ───────────── Уровень 2: Diagnostic ─────────────
         case SCREEN_BUTTONS_DIAG:
             return updateScreenButtonsDiag();
         case SCREEN_BATTERY_DIAG:
             return updateScreenBatteryDiag();
-        case SCREEN_ABOUT:
-            return updateAboutScreen();
+
+        // ───────────── Уровень 2: Тесты энкодеров ─────────────
+        case SCREEN_ENCODER_TEST_TTL:
+            return updateScreenEncoderTTLTest();
+
         default:
             return SCREEN_MAIN_MENU;
     }
